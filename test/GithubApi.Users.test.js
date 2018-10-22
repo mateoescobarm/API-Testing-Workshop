99cfa17a03b891a10e5d3024594047acc93fab81
mateo <mateoescobarm@gmail.com>
2018-10-21 19:04:27 -0500

    query parameters ok!

--- test/GithubApi.Users.test.js
+++ test/GithubApi.Users.test.js
@@ -16,4 +16,30 @@ describe('when we look for github users', () => {
   it('then response time should be less than 5 seconds', () => {
     expect(timeResponse).to.be.below(5000);
   });
+
+  describe('when i try to get 10 user per page', () => {
+    const tenUsersQuery = { per_page: 10 };
+    let userList;
+
+    before(() => agent.get(`${urlBase}`)
+      .query(tenUsersQuery).then((response) => {
+        userList = response;
+      }));
+
+    it('then there must be 10 users', () => {
+      expect(userList.body.length).to.equal(tenUsersQuery.per_page);
+    });
+    describe('when i try to get 50 user per page', () => {
+      const fiftenUsersQuery = { per_page: 50 };
+
+      before(() => agent.get(`${urlBase}`)
+        .query(fiftenUsersQuery).then((response) => {
+          userList = response;
+        }));
+
+      it('then there must be 50 users', () => {
+        expect(userList.body.length).to.equal(fiftenUsersQuery.per_page);
+      });
+    });
+  });
 });
